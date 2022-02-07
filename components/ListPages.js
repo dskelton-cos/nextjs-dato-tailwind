1c079d582519107d6683b0b12b9a1e6eff2364dc
David Skelton <dskelton@cityofsydney.nsw.gov.au>
2022-02-07 05:24:43 +0000

    working dato gql

---
+++ components/ListPages.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+export default function ListPages({ pages }) {
+
+  return (
+    <ul>
+
+      {pages.map((item) => (
+        <li key={`nav-${item.id}`}>
+          <Link href="/">
+            <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">{item.title}</a>
+          </Link>
+        </li>
+      )
+
+      )}
+    </ul>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,36 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import { request } from "../lib/datocms";
+
+import ListPages from '../components/ListPages';
+ 
+const HOMEPAGE_QUERY = `{
+  allPages {
+    id
+    title
+    _status
+    _firstPublishedAt
+    permalink
+  }
+}`;
+
+
+export async function getStaticProps({ preview = false }) {
+
+  const data = await request({
+    query: HOMEPAGE_QUERY,
+    variables: { limit: 10 },
+    preview
+  });
+  
+  return {
+    props: { data }
+  };
+}
+
+export default function Index({ data }) {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,39 +45,15 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <ListPages pages={data.allPages}/>
+        
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deployed &rarr;</h2>
-            <p>
-              After running, this repl is automatically deployed on replit!
-            </p>
-          </a>
+         
         </div>
       </main>
 
